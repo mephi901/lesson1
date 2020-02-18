1a9637f09096849b52dab8e300dfb1004390772e
Bakdoolot <algyr.alga@gmail.com>
2020-02-18 13:19:37 +0300

    Update Lesson1.java

--- lesson1/src/lesson1/Lesson1.java
+++ lesson1/src/lesson1/Lesson1.java
@@ -15,7 +15,7 @@ public class Lesson1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Hello, World!");
     }
     
 }
