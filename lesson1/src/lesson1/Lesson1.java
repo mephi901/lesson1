fdcb860a5d54ae96555631c8f75450661c9eabf2
Bakdoolot <algyr.alga@gmail.com>
2020-02-18 13:33:09 +0300

    Update Lesson1.java

--- lesson1/src/lesson1/Lesson1.java
+++ lesson1/src/lesson1/Lesson1.java
@@ -16,6 +16,7 @@ public class Lesson1 {
      */
     public static void main(String[] args) {
        System.out.println("Hello, World!");
+       System.out.println("BAKDOOLOT S DNEM ROJDENIA");
     }
     
 }
